04c146f1028a7c6274b932eccacd2face4c52298
Daniel Kruse <daniel.kruse@gmail.com>
2015-06-29 14:22:38 -0400

    Small changes to fwdkin and robotjacobian
    Modified a few under-the-hood aspects.  Should work as before

--- fwdkin.m
+++ fwdkin.m
@@ -1,54 +1,45 @@
-
-function [R,p]=fwdkin(robot,theta)
+function [R, p] = fwdkin(kin, theta)
     % FWDKIN
     %
-    % [R, p]=fwdkin(robot, theta)
+    % [R, p] = fwdkin(kin, theta)
     %
     % purpose: general forward kinematics for serial chain robot
     %           (compatible with matlab-rigid-body-viz toolbox)
     % 
     % input:
-    %   robot: struct with form:
+    %   kin: kinematics struct with form:
     %       root
-    %           -> kin
-    %               -> H          : [h_1 h_2 ... h_n]  
-    %                               (3 x n) actuator axes
-    %               -> P          : [p_{0,1} p_{1,2} ... p_{n-1,n}] 
-    %                               (3 x n + 1) actuator displacements
-    %               -> joint_type : n-vector of joint types
-    %                               0 - rotational
-    %                               1 - prismatic
-    %                               2 - mobile orientation
-    %                               3 - mobile translation
+    %           -> H          : [h_1 h_2 ... h_n]  
+    %                           (3 x n) actuator axes
+    %           -> P          : [p_{O,1} p_{1,2} ... p_{n,T}] 
+    %                           (3 x n + 1) actuator displacements
+    %           -> joint_type : n-vector of joint types
+    %                           0 - rotational
+    %                           1 - prismatic
+    %                           2 - mobile orientation
+    %                           3 - mobile translation
     %   
     %   theta: n-vector of actuator state (expecting radians for
     %                                           rotational states)
     % 
     % output:
-    %       R = R_{0,n}: 3 x 3 matrix for orientation of end effector with
+    %       R = R_{O,T}: 3 x 3 matrix for orientation of end effector with
     %                   respect to base frame
-    %       p = p_{0,n}: 3 x 1 vector giving position of end effector with
+    %       p = p_{O,T}: 3 x 1 vector giving position of end effector with
     %                   respect to base frame in base frame coordinates
     %
-
-    if ~isfield(robot,'kin')
-        error('fwdkin:fieldnotfound', ...
-                'Could not find field kin in robot structure');
-    end
-    
-    P=robot.kin.P;
-    type=robot.kin.joint_type;
-    H=robot.kin.H;
-    
-    p = P(:,1);
+        
+    p = kin.P(:,1);
     R = eye(3);
 
-    for i = 1:numel(type)
-        if any(type(i) == [0 2])        % rotational actuators
-            R = R*rot(H(:,i),theta(i));
-        elseif any(type(i) == [1 3])    % translational actuators
-            p = p + R*H(:,i)*theta(i);
+    for i = 1:numel(kin.joint_type)
+        if (kin.joint_type(i) == 0 || ...       % rotational actuators
+                    kin.joint_type(i) == 2)        
+            R = R*rot(kin.H(:,i),theta(i));
+        elseif (kin.joint_type(i) == 1 || ...   % translational actuators
+                    kin.joint_type(i) == 3)    
+            p = p + R*kin.H(:,i)*theta(i);
         end
-        p = p + R*P(:,i+1);
+        p = p + R*kin.P(:,i+1);
     end
 end
--- robotjacobian.m
+++ robotjacobian.m
@@ -1,71 +1,68 @@
-function J=robotjacobian(robot,theta)
+function J = robotjacobian(kin, theta)
     % ROBOTJACOBIAN
     %
-    % J = robotjacobian(robot, theta)
+    % J = robotjacobian(kin, theta)
     %
     % purpose: calculate jacobian for serial chain robot relating joint
     %           velocity to end effector spatial velocity
     %           (compatible with matlab-rigid-body-viz toolbox)
     %
     % input:
-    %   robot: struct with form:
+    %   kin: struct with form:
     %       root
-    %           -> kin
-    %               -> H          : [h_1 h_2 ... h_n]  
-    %                               (3 x n) actuator axes
-    %               -> P          : [p_{0,1} p_{1,2} ... p_{n-1,n}] 
-    %                               (3 x n + 1) actuator displacements
-    %               -> joint_type : n-vector of joint types
-    %                               0 - rotational
-    %                               1 - prismatic
-    %                               2 - mobile orientation
-    %                               3 - mobile translation
+    %           -> H          : [h_1 h_2 ... h_n]  
+    %                           (3 x n) actuator axes
+    %           -> P          : [p_{O,1} p_{1,2} ... p_{n,T}] 
+    %                           (3 x n + 1) actuator displacements
+    %           -> joint_type : n-vector of joint types
+    %                           0 - rotational
+    %                           1 - prismatic
+    %                           2 - mobile orientation
+    %                           3 - mobile translation
     %   
     %   theta: n-vector of actuator position (expecting radians for
     %                                           rotational states)
     % 
     % output:
-    %       J = [jw_1 jw_2 ... jw_n] (6 x n) jacobian matrix according to
-    %           [jv_1 jv_2 ... jv_n]        [d-w/d-theta; d-v/d-theta]
-    %                                       
+    %       J = [jw_1 jw_2 ... jw_n] (6 x n) jacobian matrix 
+    %           [jv_1 jv_2 ... jv_n]        * in base frame
+    %           top three rows reflect the instantaneous change in 
+    %           end effector angular velocity while the bottom three rows
+    %           are the instantaneous change in linear velocity both with
+    %           respect to actuator velocity
     %
     
-    if ~isfield(robot,'kin')
-        error('robotjacobian:fieldnotfound', ...
-                'Could not find field kin in robot structure');
-    end
-    
-    P=robot.kin.P;
-    type=robot.kin.type;
-    H=robot.kin.H;
-    
-    p = P(:,1);
+    p = kin.P(:,1);
     R = eye(3);
     
-    J=zeros(6,numel(type));
+    J = zeros(6,numel(kin.joint_type));
 
     hi = zeros(3,n);
     pOi = zeros(3,n+1);
     pOi(:,1) = p;
 
-    % Store forward kinematics
+    % Compute and store forward kinematics
     for i = 1:n
-        if any(type(i) == [0 2])        % rotational actuators
-            R = R*rot(H(:,i),theta(i));
-            p = p + R*P(:,i+1);
-        elseif any(type(i) == [1 3])    % translational actuators
-            p = p + R*(H(:,i)*theta(i) + P(:,i+1));
+        if (kin.joint_type(i) == 0 || ...       % rotational actuators
+                    kin.joint_type(i) == 2)        
+            R = R*rot(kin.H(:,i),theta(i));
+        elseif (kin.joint_type(i) == 1 || ...   % translational actuators
+                    kin.joint_type(i) == 3) 
+            p = p + R*kin.H(:,i)*theta(i);
         end
+        p = p + R*kin.P(:,i+1);
         pOi(:,i+1) = p;
-        hi(:,i) = R*H(:,i);
+        hi(:,i) = R*kin.H(:,i);
     end
 
     p0T = pOi(:,end);
     % Compute Jacobian
     for i=1:n
-        if any(type(i) == [0 2])        % rotational actuators
+        if (kin.joint_type(i) == 0 || ...       % rotational actuators
+                kin.joint_type(i) == 2)        
             J(:,i)=[hi(:,i); hat(hi(:,i))*(p0T - pOi(:,i))];
-        elseif any(type(i) == [1 3])    % translational actuators
+        elseif (kin.joint_type(i) == 1 || ...   % translational actuators
+                kin.joint_type(i) == 3)   
             J(:,i)=[0;0;0; hi(:,i)];
         end
     end
