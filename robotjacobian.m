832139f708ee596a3343074aa8bdffdc6ddd7f02
Daniel Kruse <daniel.kruse@gmail.com>
2015-09-07 17:36:29 -0400

    jacobian fix for nonholonomic robot
    nonholonomic jacobian columns were missing a major entry, fixed.

--- robotjacobian.m
+++ robotjacobian.m
@@ -55,21 +55,23 @@ function J = robotjacobian(kin, theta)
         hi(:,i) = R*kin.H(:,i);
     end
 
-    p0T = pOi(:,end);
+    pOT = pOi(:,end);
     % Compute Jacobian
     i = 1;
     j = 1;
     while i <= numel(kin.joint_type)
         if kin.joint_type(i) == 0               % revolute actuators
-            J(:,j) = [hi(:,i); hat(hi(:,i))*(p0T - pOi(:,i))];
+            J(:,j) = [hi(:,i); hat(hi(:,i))*(pOT - pOi(:,i))];
         elseif kin.joint_type(i) == 1           % prismatic actuators  
             J(:,j) = [0;0;0; hi(:,i)];
         elseif kin.joint_type(i) == 3           % nonholonomic mobile
             % This is a special case, and is dependent on the next
             % two 'joints' following the format for the unicycle model.  
             % Should consider new format in future release.
-            J(:,j) = [0;0;0;rot(hi(:,i+2),theta(i+2))*hi(:,i)];
-            J(:,j+1) = [hi(:,i+2);0;0;0];
+            % Linear Velocity
+            J(:,j) = [0;0;0;rot(hi(:,i+2),theta(i+2))*hi(:,i+1)];
+            % Angular Velocity
+            J(:,j+1) = [hi(:,i+2);hat(hi(:,i+2))*(pOT - pOi(:,i+2))];
             J = J(:,1:end-1);
             i = i + 2;
             j = j + 1;
