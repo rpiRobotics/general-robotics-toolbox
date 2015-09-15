474765b395ff8c638685cb93f66358e69b01e675
Daniel Kruse <daniel.kruse@gmail.com>
2015-09-15 12:35:28 -0400

    Added quaternion product
    Quaternion product operator

---
+++ quatproduct.m
@@ -0,0 +1,14 @@
+function Q = quatproduct(q)
+    % QUATPRODUCT
+    %
+    % Q = quatproduct(q)
+    %
+    % generates matrix representation of a Hamilton quaternion product
+    % operator
+    %
+    % in: q = [q0;qv];
+    % out: Q = [q0 qv'; -qv q0*eye(3)+ cross(qv)]
+    
+    Q = [q(1) q(2:4)'; -q(2:4) q(1)*eye(3)+hat(qv(2:4))];
+    
+end
